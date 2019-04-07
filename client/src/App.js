44c1a1877ccebcd1dbe5dfd5e7baa0feebbc05d8
Selman Kahya <selmankahya@hotmail.com>
2019-04-06 19:43:31 -0700

    url

--- client/src/App.js
+++ client/src/App.js
@@ -332,8 +332,8 @@ class App extends Component {
   }
 
   start = async () => {
-    var socket = io('http://localhost:5000');
-    // var socket = io('https://selman-123.herokuapp.com');
+    // var socket = io('http://localhost:5000');
+    var socket = io('https://selman-123.herokuapp.com');
     socket.emit('PLAYER_NAME_UPDATE', { name: this.state.name });
     if (!this.state.isGameRunning) {
       this.game = new Game(this.getCtx(), socket);
